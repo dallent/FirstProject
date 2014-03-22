1bf56b25dbde8ffb1e380d8d9f4affe7b1005ac0
dallent <daveandlisag@cox.net>
2014-03-22 15:29:03 -0700

    Java Tutorial Series - Patrick WashingtonDC

---
+++ ExampleIF.java
@@ -0,0 +1,22 @@
+package day1.examples;
+
+/**
+ * Created by ${Dallent} on 3/22/14.
+ */
+public class ExampleIF {
+
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 20;
+
+        if (x < y) {
+            System.out.println("X is less than Y");
+        }
+
+        if (x == y) {
+            System.out.println("X is equal to Y");
+        }
+
+        if (x > y)
+    }
+}
--- MyFirstClass.java
+++ MyFirstClass.java
@@ -11,8 +11,14 @@ public class MyFirstClass {
     public static void main(String[] args) {
 
             //My First Statement
-            System.out.println("Hello World");
+        System.out.println("Hello World");
 
+            int x = 10;
+            int y = 20;
+
+            int result = x + y;
+
+            System.out.println(result);
 
 
     }
